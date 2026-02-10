d5174096d64bd07486eb7958d21705325ab49ce8
Bishal Paudel <bishalpaudel1998@gmail.com>
2026-02-10 07:08:46 -0500

    PatientMapper updated for Request to Patient entity conversion

--- patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -1,5 +1,6 @@
 package com.pm.patientservice.mapper;
 
+import com.pm.patientservice.dto.PatientRequestDto;
 import com.pm.patientservice.dto.PatientResponseDto;
 import com.pm.patientservice.model.Patient;
 
@@ -16,9 +17,15 @@ public class PatientMapper {
         );
     }
 
-    public static Patient toPatient(PatientResponseDto responseDto){
-        return new Patient(
+    public static Patient toPatient(PatientRequestDto request){
+        Patient newPatient = new Patient();
 
-        );
+        newPatient.setName(request.name());
+        newPatient.setEmail(request.email());
+        newPatient.setAddress(request.address());
+        newPatient.setDateOfBirth(request.dateOfBirth());
+        newPatient.setRegisteredDate(request.registeredDate());
+
+        return newPatient;
     }
 }
