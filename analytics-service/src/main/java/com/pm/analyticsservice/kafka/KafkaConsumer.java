d93cfcb536301691d8be21dd3cc5574acd590a7f
Bishal Paudel <bishalpaudel1998@gmail.com>
2026-02-23 23:31:29 -0500

    DeSerializers environment variables added to the docker-compose and Payload annotation added to the KafkaConsumer

--- analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
@@ -4,6 +4,7 @@ import com.google.protobuf.InvalidProtocolBufferException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 import patient.events.PatientEvent;
 
@@ -13,12 +14,12 @@ public class KafkaConsumer {
     private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
 
     @KafkaListener(topics = "patient", groupId = "analytics-service")
-    public void consumeEvent(byte[] event) throws InvalidProtocolBufferException {
+    public void consumeEvent(@Payload byte[] event) throws InvalidProtocolBufferException {
 
         try{
             PatientEvent patientEvent = PatientEvent.parseFrom(event);
 
-            log.info("Received Patient Event : [ PatientId : {}, PatientName: {}, PatientEmail: {}]",
+            log.info("Received Patient Event : [PatientId={}, PatientName={}, PatientEmail={}]",
                     patientEvent.getPatientId(),
                     patientEvent.getName(),
                     patientEvent.getEmail());
