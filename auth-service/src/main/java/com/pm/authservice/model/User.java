72ea09fcb128e3e70f044f2a18b0ab4d7abf5f87
Bishal Paudel <bishalpaudel1998@gmail.com>
2026-03-02 06:50:19 -0500

    User model created for auth-service

---
+++ auth-service/src/main/java/com/pm/authservice/model/User.java
@@ -0,0 +1,30 @@
+package com.pm.authservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private Email email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
