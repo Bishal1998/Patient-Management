db11431254844e38e730f2f68956f457acb15fef
Bishal Paudel <bishalpaudel1998@gmail.com>
2026-03-04 10:44:34 -0500

    JwtUtil created to generate jwt token

--- auth-service/src/main/java/com/pm/authservice/service/impl/AuthServiceImpl.java
+++ auth-service/src/main/java/com/pm/authservice/service/impl/AuthServiceImpl.java
@@ -4,6 +4,7 @@ import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.model.User;
 import com.pm.authservice.repository.AuthRepository;
 import com.pm.authservice.service.AuthService;
+import com.pm.authservice.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ public class AuthServiceImpl implements AuthService {
 
     private final AuthRepository authRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     @Override
     public Optional<String> login(LoginRequestDTO loginRequestDTO) {
